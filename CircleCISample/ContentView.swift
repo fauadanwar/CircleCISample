9958060d41861b30cd82017ba01f2ee6a50c110c
fauadanwar <fauad.anwar@gmail.com>
2023-08-24 18:57:44 +0530

    - Re-adding project

---
+++ CircleCISample/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  CircleCISample
+//
+//  Created by fanwar on 24/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var displayName: String
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text(displayName)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(displayName: "Hello, Preview!")
+    }
+}
+
