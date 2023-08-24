3bd89babfa1af65efeb1b4b3b9a180b8ab2029cd
fauadanwar <fauad.anwar@gmail.com>
2023-08-24 16:27:14 +0530

    - Added project.

---
+++ CircleCISample/CircleCISample/CircleCISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleCISampleApp.swift
+//  CircleCISample
+//
+//  Created by fanwar on 24/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CircleCISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView(displayName: "Hello, world!")
+        }
+    }
+}
